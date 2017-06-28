a8beaab2abca70e51f0f6dac7199297de4c74dd6
alexisgstalter <a.gstalter@junglogistique.fr>
2017-06-28 10:24:15 +0200

    installation materialize + menuing de base

--- SiteFoot/App_Start/BundleConfig.cs
+++ SiteFoot/App_Start/BundleConfig.cs
@@ -20,11 +20,11 @@ namespace SiteFoot
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/materialize.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/materialize.css",
                       "~/Content/site.css"));
         }
     }
