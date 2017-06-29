0b288ec2969506f37eedf5c65033fd3a8bab21f3
Brunstein Julien <j.brunstein@junglogistique.fr>
2017-06-29 11:45:08 +0200

    commit

--- SiteFoot/Controllers/AccountController.cs
+++ SiteFoot/Controllers/AccountController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using SiteFoot.Models;
-
+ 
 namespace SiteFoot.Controllers
 {
     [Authorize]
