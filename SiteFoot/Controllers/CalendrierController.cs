6368405ec9113ced31b6b3f6ef613213386dfc5c
Brunstein Julien <j.brunstein@junglogistique.fr>
2017-07-04 12:13:10 +0200

    commit

--- SiteFoot/Controllers/CalendrierController.cs
+++ SiteFoot/Controllers/CalendrierController.cs
@@ -73,6 +73,61 @@ namespace SiteFoot.Controllers
                 return Json(new { ok = false, error = e.Message });
             }
         }
+
+
+
+        public String GetEventsEntrainement(string start, string end)
+        {
+            try
+            {
+                DataTable dt = CalendrierManager.GetEventsEntrainement(start, end);
+                dt.Columns[3].ColumnName = "end";
+                DataTable dtCloned = dt.Clone();
+                dtCloned.Columns[0].DataType = typeof(String);
+                dtCloned.Columns[4].DataType = typeof(Boolean);
+                foreach (DataRow row in dt.Rows)
+                {
+                    dtCloned.ImportRow(row);
+                }
+
+                string result = JsonConvert.SerializeObject(dtCloned);
+
+                result = result.Replace("\\", "");
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return "";
+            }
+        }
+
+        public JsonResult GetEntrainement(int id)
+        {
+            try
+            {
+                DataTable ev = CalendrierManager.GetEvententrainementById(id);
+                if (ev.Rows.Count > 0)
+                {
+                    return Json(new { ok = true, hasResult = true, id = ev.Rows[0]["equipe"].ToString(), terrain = ev.Rows[0]["terrain"].ToString(), heure_debut = DateTime.Parse(ev.Rows[0]["start"].ToString()).ToString("dd/MM/yyyy HH:mm:ss"), heure_fin = DateTime.Parse(ev.Rows[0]["fin"].ToString()).ToString("dd/MM/yyyy HH:mm:ss") });
+                }
+                else
+                {
+                    return Json(new { ok = true, hasResult = false });
+                }
+            }
+            catch (Exception e)
+            {
+                return Json(new { ok = false, error = e.Message });
+            }
+        }
+
+
+
+
+
+
     }
 
 }
--- SiteFoot/Façades/CalendrierManager.cs
+++ SiteFoot/Façades/CalendrierManager.cs
@@ -48,5 +48,35 @@ namespace SiteFoot.Façades
             cmd.Parameters.Add("@responsable", SqlDbType.VarChar).Value = responsable;
             cmd.ExecuteNonQuery();
         }
+
+
+
+
+        public static DataTable GetEventsEntrainement(String date_debut, String date_fin)
+        {
+            String connectionString = ConfigurationManager.ConnectionStrings["SQLSiteWeb"].ToString(); //Récupération de la chaîne de connexion
+            SqlConnection myConnection = new SqlConnection(connectionString); //Nouvelle connexion à la base de donnée
+            myConnection.Open(); //On ouvre la connexion
+            SqlDataAdapter source = new SqlDataAdapter("select * from entrainement where start > @date_debut and fin < @date_fin", myConnection);
+            source.SelectCommand.Parameters.Add("@date_debut", SqlDbType.DateTime).Value = DateTime.Parse(date_debut);
+            source.SelectCommand.Parameters.Add("@date_fin", SqlDbType.DateTime).Value = DateTime.Parse(date_fin);
+            DataTable data = new DataTable();
+            source.Fill(data);
+            myConnection.Close();
+            return data;
+        }
+        public static DataTable GetEventBuvetteById(int id)
+        {
+            String connectionString = ConfigurationManager.ConnectionStrings["SQLSiteWeb"].ToString(); //Récupération de la chaîne de connexion
+            SqlConnection myConnection = new SqlConnection(connectionString); //Nouvelle connexion à la base de donnée
+            myConnection.Open(); //On ouvre la connexion
+            SqlDataAdapter source = new SqlDataAdapter("select * from TestBuvette where id=@id", myConnection);
+            source.SelectCommand.Parameters.Add("@id", SqlDbType.Int).Value = id;
+            DataTable data = new DataTable();
+            source.Fill(data);
+            myConnection.Close();
+            return data;
+        }
+
     }
 }
