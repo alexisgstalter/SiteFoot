5522d8c150e46ed7d01df812a66f53f95d8a31d2
alexisgstalter <a.gstalter@junglogistique.fr>
2017-07-04 10:24:43 +0200

    calendrier buvette

--- SiteFoot/Controllers/CalendrierController.cs
+++ SiteFoot/Controllers/CalendrierController.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SiteFoot.Façades;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace SiteFoot.Controllers
 {
+    [CustomAuthorize(GroupeAllow="ADMIN")]
     public class CalendrierController : Controller
     {
         // GET: Calendrier
@@ -21,5 +27,52 @@ namespace SiteFoot.Controllers
         {
             return View();
         }
+        public String GetEventsBuvette(string start, string end)
+        {
+            try
+            {
+                DataTable dt = CalendrierManager.GetEventsBuvette(start,end);
+                dt.Columns[3].ColumnName = "end";
+                DataTable dtCloned = dt.Clone();
+                dtCloned.Columns[0].DataType = typeof(String);
+                dtCloned.Columns[4].DataType = typeof(Boolean);
+                foreach (DataRow row in dt.Rows)
+                {
+                    dtCloned.ImportRow(row);
+                }
+                
+                string result = JsonConvert.SerializeObject(dtCloned);
+
+                result = result.Replace("\\","");
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return "";
+            }
+        }
+
+        public JsonResult GetEvent(int id)
+        {
+            try
+            {
+                DataTable ev = CalendrierManager.GetEventBuvetteById(id);
+                if (ev.Rows.Count > 0)
+                {
+                    return Json(new { ok = true, hasResult = true, id = ev.Rows[0]["title"].ToString(), titre = ev.Rows[0]["title"].ToString(), responsable = ev.Rows[0]["utilisateur"].ToString(), heure_debut = DateTime.Parse(ev.Rows[0]["start"].ToString()).ToString("dd/MM/yyyy HH:mm:ss"), heure_fin = DateTime.Parse(ev.Rows[0]["fin"].ToString()).ToString("dd/MM/yyyy HH:mm:ss") });
+                }
+                else
+                {
+                    return Json(new { ok = true, hasResult = false });
+                }
+            }
+            catch (Exception e)
+            {
+                return Json(new { ok = false, error = e.Message });
+            }
+        }
     }
+
 }
---
+++ SiteFoot/Façades/CalendrierManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SiteFoot.Façades
+{
+    public class CalendrierManager
+    {
+        public static DataTable GetEventsBuvette(String date_debut, String date_fin)
+        {
+            String connectionString = ConfigurationManager.ConnectionStrings["SQLSiteWeb"].ToString(); //Récupération de la chaîne de connexion
+            SqlConnection myConnection = new SqlConnection(connectionString); //Nouvelle connexion à la base de donnée
+            myConnection.Open(); //On ouvre la connexion
+            SqlDataAdapter source = new SqlDataAdapter("select * from TestBuvette where start > @date_debut and fin < @date_fin", myConnection);
+            source.SelectCommand.Parameters.Add("@date_debut", SqlDbType.DateTime).Value = DateTime.Parse(date_debut);
+            source.SelectCommand.Parameters.Add("@date_fin", SqlDbType.DateTime).Value = DateTime.Parse(date_fin);
+            DataTable data = new DataTable();
+            source.Fill(data);
+            myConnection.Close();
+            return data;
+        }
+        public static DataTable GetEventBuvetteById(int id)
+        {
+            String connectionString = ConfigurationManager.ConnectionStrings["SQLSiteWeb"].ToString(); //Récupération de la chaîne de connexion
+            SqlConnection myConnection = new SqlConnection(connectionString); //Nouvelle connexion à la base de donnée
+            myConnection.Open(); //On ouvre la connexion
+            SqlDataAdapter source = new SqlDataAdapter("select * from TestBuvette where id=@id", myConnection);
+            source.SelectCommand.Parameters.Add("@id", SqlDbType.Int).Value = id;
+            DataTable data = new DataTable();
+            source.Fill(data);
+            myConnection.Close();
+            return data;
+        }
+        public static void UpdateEventBuvette(int id, String titre, String responsable, DateTime date_debut, DateTime date_fin)
+        {
+            String connectionString = ConfigurationManager.ConnectionStrings["SQLSiteWeb"].ToString(); //Récupération de la chaîne de connexion
+            SqlConnection myConnection = new SqlConnection(connectionString); //Nouvelle connexion à la base de donnée
+            myConnection.Open(); //On ouvre la connexion
+            SqlCommand cmd = new SqlCommand("update TestEvent set title=@titre, start=@date_debut, fin=@date_fin, utilisateur=@responsable where id=@id");
+            cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
+            cmd.Parameters.Add("@date_debut", SqlDbType.DateTime).Value = date_debut;
+            cmd.Parameters.Add("@date_fin", SqlDbType.DateTime).Value = date_fin;
+            cmd.Parameters.Add("@titre", SqlDbType.VarChar).Value = titre;
+            cmd.Parameters.Add("@responsable", SqlDbType.VarChar).Value = responsable;
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
